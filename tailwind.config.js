0fab27b16f2984f5201cd8cde33b7d649d1db877
Vincent Chen <vincentchen@VincentdeMacBook-Air.local>
2023-10-20 10:40:44 +0800

    建構配置

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2B4AE",
+        secondary: "#F2DCC9",
+        pink: "#F29188",
+      },
+    },
+  },
+  plugins: [],
+};
